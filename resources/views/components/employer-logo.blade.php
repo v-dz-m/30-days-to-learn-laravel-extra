50e27cc6a0bc9b7b355152a58ecdaba0444e6640
Vadzim Hanchuk <31338180+v-dz-m@users.noreply.github.com>
2024-11-29 10:59:46 +0300

    Add day no. 30

--- resources/views/components/employer-logo.blade.php
+++ resources/views/components/employer-logo.blade.php
@@ -1,3 +1,3 @@
-@props(['width' => 90])
+@props(['employer', 'width' => 90])
 
-<img src="http://placehold.it/{{ $width }}/{{ $width }}" alt="" class="rounded-xl">
+<img src="{{ asset($employer->logo) }}" alt="" class="rounded-xl" width="{{ $width }}" height="{{ $width }}">
